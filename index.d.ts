daf67f78401d441e2769b97e0018e258e04ca232
5antos <a67984@ualg.pt>
2021-05-09 15:48:12 +0100

    Updating

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+import { Data } from './src/Utils';
+export declare class SimplDB {
+    private readonly config;
+    private data;
+    constructor(config: {
+        filePath: string;
+        saveOnUpdate: boolean;
+        tabSize: number;
+    });
+    private checkJSON;
+    private validatePath;
+    private fetchData;
+    private addOrSubtract;
+    set(key: string, value: any): void;
+    add(key: string, value: number): number | never;
+    subtract(key: string, value: number): number | never;
+    push(key: string, value: any): Data;
+    pull(key: string, value: any): void;
+    get(key: string): any;
+    has(key: string): boolean;
+    delete(key: string): boolean;
+    clear(): void;
+    toJSON(): JSON | never;
+    save(): void;
+}
