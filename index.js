90e8db47781e00130df13a50d0a3b0075b48900b
5antos <a67984@ualg.pt>
2021-05-13 15:46:03 +0100

    Removed unnecessary code

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
 "use strict";
 
-const Database = require("./lib/Database");
-
-function SimplDB(config) {
-  return new Database(config);
-}
-
-SimplDB.Database = Database;
-
-module.exports = SimplDB;
+module.exports = require("./lib/Database");
