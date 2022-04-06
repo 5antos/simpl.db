6319e3124ee27ff197d8d7bc3d5b9567cea80ab9
5antos <a67984@ualg.pt>
2022-04-06 15:35:27 +0100

    Added and renamed tests

--- tests/Database.test.js
+++ tests/Database.test.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-undef */
+
 const Database = require('../lib/Database');
 const db = new Database({ autoSave: false });
 
@@ -13,27 +14,27 @@ jest.mock('fs', () => {
   };
 });
 
-test('if has function is working', () => {
 
+test('Database#has', () => {
   expect(db.has('dessert')).toBe(false);
 
   db.set('dessert', 'cake');
 
   expect(db.has('dessert')).toBe(true);
   expect(() => db.has([])).toThrow(/key is invalid/);
-
 });
 
-test('if set function is working', () => {
-
+test('Database#set', () => {
   expect(db.set('key', 'value')).toBe('value');
   expect(() => db.set({}, 'object')).toThrow(/key is invalid/);
   expect(() => db.set('key.', 'value')).toThrow();
   expect(() => db.set('aa', 'bb', {})).toThrow('Parameter encrypt must be of type boolean');
 
+  // TODO
+  // Test encryption
 });
 
-test('if get function is working', () => {
+test('Database#get', () => {
 
   expect(db.get('system')).toBe(undefined);
   db.set('system', 'Zorin OS');
@@ -45,25 +46,23 @@ test('if get function is working', () => {
   expect(() => db.set('key', 'value', true)).toThrow('Missing Encryption Key');
 
   // TODO
-  // Test encryption
+  // Test decryption
 
 });
 
-test('if add function is working', () => {
-  
+test('Database#add', () => {
   db.set('items', 10);
+
   expect(db.add('items', -20)).toBe(-10);
   expect(() => db.add('items', Infinity)).toThrow('A valid value must be provided');
   expect(() => db.add('items', NaN)).toThrow();
   expect(() => db.add('items', undefined)).toThrow();
-
 });
 
-test('if subtract function is working', () => {
-
+test('Database#subtract', () => {
   db.set('items', 0);
+
   expect(db.subtract('items', 20)).toBe(-20);
   expect(db.subtract('items', 0)).toBe(-20);
   expect(() => db.subtract('items', Infinity)).toThrow();
-
 });
