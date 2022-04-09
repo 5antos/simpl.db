0cf089a0d7d52674efa0c8f18f46e278b6fda21f
5antos <a67984@ualg.pt>
2022-04-09 20:13:26 +0100

    Covered all Database methods with tests

--- tests/Database.test.js
+++ tests/Database.test.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-undef */
 
 const Database = require('../lib/Database');
-const db = new Database({ autoSave: false });
+const Collection = require('../lib/Collection');
+const db = new Database({ autoSave: false, encryptionKey: 'n2dE3cU2UjVfhHGhmTaatrzcpVF6JLbu' });
 
 
 jest.mock('fs', () => {
@@ -15,77 +16,192 @@ jest.mock('fs', () => {
 });
 
 
+
+test('Database#add', () => {
+  db.set('balance', 350);
+  db.set('account', { owner: '5antos', balance: 800 });
+
+  expect(db.add('balance', 500)).toBe(850);
+  expect(db.add('account.balance', 200)).toEqual({ owner: '5antos', balance: 1000 });
+
+  expect(() => db.add('balance', Infinity)).toThrow('A valid value must be provided');
+  expect(() => db.add('balance', NaN)).toThrow('A valid value must be provided');
+  expect(() => db.add('account..balance', 200)).toThrow(/key is invalid/);
+
+  db.clear();
+});
+
+
+test('Database#clear', () => {
+  db.set('people', { users: [{ name: 'Peter' }, { name: 'Michael' }] });
+  db.clear();
+
+  expect(db.get('people')).toBe(undefined);
+  expect(db.toJSON()).toEqual({});
+});
+
+
 test('Database#createCollection', () => {
   const collections = db.collections.length;
-  const collection = db.createCollection('myCollection');
+  const collection = db.createCollection('users');
 
   expect(collection.entries).toBe(0);
   expect(collections).toBe(db.collections.length - 1);
+
+  expect(() => db.createCollection('users')).toThrow('A collection with the provided name already exists');
+  expect(() => db.createCollection(null)).toThrow(/name is invalid/);
+
+  db.collections = [];
 });
 
-test('Database#has', () => {
-  expect(db.has('dessert')).toBe(false);
 
-  db.set('dessert', 'cake');
+test('Database#delete', () => {
+  db.set('food', ['pizza', 'burger', 'fries']);
 
-  expect(db.has('dessert')).toBe(true);
-  expect(() => db.has([])).toThrow(/key is invalid/);
+  expect(db.delete('food')).toBe(true);
+  expect(db.delete('drinks')).toBe(false);
+  expect(db.get('food')).toBe(undefined);
+
+  expect(() => db.delete('some..key')).toThrow(/key is invalid/);
+
+  db.clear();
 });
 
-test('Database#set', () => {
-  expect(db.set('key', 'value')).toBe('value');
-  expect(() => db.set({}, 'object')).toThrow(/key is invalid/);
-  expect(() => db.set('key.', 'value')).toThrow();
-  expect(() => db.set('aa', 'bb', {})).toThrow('Parameter encrypt must be of type boolean');
 
-  // TODO
-  // Test encryption
+test('Database#deleteCollection', () => {
+  db.createCollection('users');
+
+  expect(db.deleteCollection('posts')).toBe(false);
+  expect(db.collections.length).toBe(1);
+
+  expect(db.deleteCollection('users')).toBe(true);
+  expect(db.collections.length).toBe(0);
+
+  expect(() => db.deleteCollection(null)).toThrow(/name is invalid/);
+
+  db.collections = [];
 });
+
+
+test('Database#fetch', () => {}); // Cannot be tested
+
 
 test('Database#get', () => {
+  db.set('Greeting', 'Hello World');
+  db.set('user', { name: 'Peter', age: 19 });
+  db.set('PASSWORD', 'password123', true);
 
-  expect(db.get('system')).toBe(undefined);
-  db.set('system', 'Zorin OS');
+  expect(db.get('Greeting')).toBe('Hello World');
+  expect(db.get('user.name')).toBe('Peter');
+  expect(db.get('PASSWORD')).not.toBe('password123');
+  expect(db.get('PASSWORD', true)).toBe('password123');
 
-  expect(db.get('system')).toBe('Zorin OS');
-  db.set('arrNumber', [ 1, 2, 3 ]);
+  expect(() => db.get('some..key')).toThrow(/key is invalid/);
+  expect(() => db.get('user.name', null)).toThrow(/decrypt must be of type boolean/);
 
-  expect(db.get('arrNumber')).toEqual([1, 2, 3]);
-  expect(() => db.set('key', 'value', true)).toThrow('Missing Encryption Key');
-
-  // TODO
-  // Test decryption
-
+  db.clear();
 });
 
-test('Database#add', () => {
-  db.set('items', 10);
-
-  expect(db.add('items', -20)).toBe(-10);
-  expect(() => db.add('items', Infinity)).toThrow('A valid value must be provided');
-  expect(() => db.add('items', NaN)).toThrow();
-  expect(() => db.add('items', undefined)).toThrow();
-});
-
-test('Database#subtract', () => {
-  db.set('items', 0);
-
-  expect(db.subtract('items', 20)).toBe(-20);
-  expect(db.subtract('items', 0)).toBe(-20);
-  expect(() => db.subtract('items', Infinity)).toThrow();
-});
-
-test('Database#pull', () => {
-  db.push('array1', { name: 'John' });
-  db.push('array2', 3);
-
-  expect(db.pull('array1', { name: 'John' })).toEqual([]);
-  expect(db.pull('array2', 3)).toEqual([]);
-});
 
 test('Database#getCollection', () => {
-  const collection = db.createCollection('collection');
+  db.createCollection('people');
 
-  expect(db.getCollection('collection')).toEqual(collection);
-  expect(db.getCollection('inexistent')).toBe(null);
+  expect(db.getCollection('people')).toBeInstanceOf(Collection);
+  expect(db.getCollection('users')).toBe(null);
+
+  expect(() => db.getCollection(null)).toThrow(/name is invalid/);
+
+  db.collections = [];
+});
+
+
+test('Database#has', () => {
+  db.set('prefix', '!');
+  db.set('profile', { username: '5antos' });
+
+  expect(db.has('prefix')).toBe(true);
+  expect(db.has('profile.username')).toBe(true);
+  expect(db.has('profile.password')).toBe(false);
+
+  expect(() => db.has('some..key')).toThrow(/key is invalid/);
+
+  db.clear();
+});
+
+
+test('Database#pull', () => {
+  db.set('numbers', [1, 2, 3, 2, 4, 3, 1, 1]);
+  db.set('people', [{ name: 'Peter' }, { name: 'Michael' }]);
+  db.set('user', { id: 32171, items: ['sword', 'axe'] });
+  db.set('key', 'value');
+
+  expect(db.pull('numbers', 1)).toEqual([2, 3, 2, 4, 3]);
+  expect(db.pull('people', { name: 'Peter' })).toEqual([{ name: 'Michael' }]);
+  expect(db.pull('user.items', 'bow')).toEqual({ id: 32171, items: ['sword', 'axe'] });
+  expect(db.pull('user.items', 'sword')).toEqual({ id: 32171, items: ['axe'] });
+
+  expect(() => db.pull('some..key', 'value')).toThrow(/key is invalid/);
+  expect(() => db.pull('numbers', undefined)).toThrow(/valid value must be provided/);
+  expect(() => db.pull('key', 'value')).toThrow(/value of the provided key must be an array/);
+
+  db.clear();
+});
+
+
+test('Database#push', () => {
+  db.set('people', ['Peter', 'Henry']);
+  db.set('user', { id: 32171, items: ['sword'] });
+  db.set('key', 'value');
+
+  expect(db.push('people', 'Catherine')).toEqual(['Peter', 'Henry', 'Catherine']);
+  expect(db.push('user.items', 'axe')).toEqual({ id: 32171, items: ['sword', 'axe'] });
+  expect(db.push('posts', { title: 'My first post' })).toEqual([{ title: 'My first post' }]);
+
+  expect(() => db.push('some..key', 'value')).toThrow(/key is invalid/);
+  expect(() => db.push('people', undefined)).toThrow(/valid value must be provided/);
+  expect(() => db.push('key', 'value')).toThrow(/value of the provided key must be an array/);
+
+  db.clear();
+});
+
+
+test('Database#save', () => {}); // Cannot be tested
+
+
+test('Database#set', () => {
+  expect(db.set('user', { name: 'Peter' })).toEqual({ name: 'Peter' });
+  expect(db.set('user.age', 20)).toEqual({ name: 'Peter', age: 20 });
+  expect(db.set('user.age', undefined)).toEqual({ name: 'Peter' });
+  expect(db.set('PASSWORD', 'password123', true)).not.toBe('password123');
+
+  expect(() => db.set('some..key', 'value')).toThrow(/key is invalid/);
+  expect(() => db.set('user.hobbies', ['Programming'], null)).toThrow(/encrypt must be of type boolean/);
+
+  db.clear();
+});
+
+
+test('Database#subtract', () => {
+  db.set('balance', 350);
+  db.set('account', { owner: '5antos', balance: 800 });
+
+  expect(db.subtract('balance', 100)).toBe(250);
+  expect(db.subtract('account.balance', 200)).toEqual({ owner: '5antos', balance: 600 });
+
+  expect(() => db.subtract('balance', Infinity)).toThrow('A valid value must be provided');
+  expect(() => db.subtract('balance', NaN)).toThrow('A valid value must be provided');
+  expect(() => db.subtract('account..balance', 200)).toThrow(/key is invalid/);
+
+  db.clear();
+});
+
+
+test('Database#toJSON', () => {
+  expect(db.toJSON()).toEqual({});
+
+  db.set('colors', ['red', 'green', 'blue']);
+
+  expect(db.toJSON()).toEqual({ colors: ['red', 'green', 'blue'] });
+
+  db.clear();
 });
